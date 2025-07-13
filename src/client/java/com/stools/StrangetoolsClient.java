b329a77b36a1c07d9df30f42abf9246a233da45a
a2743 <2743507063@qq.com>
2025-07-13 16:00:23 +0800

    Initial commit

---
+++ src/client/java/com/stools/StrangetoolsClient.java
@@ -0,0 +1,10 @@
+package com.stools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StrangetoolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/stools/StrangetoolsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.stools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StrangetoolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
